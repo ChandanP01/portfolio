3b66fe7cdc3d71345d9185125a1f93bdde9551b3
CHANDAN KUMAR PARIDA <ckparida233021@gmail.com>
2024-07-27 12:35:55 +0530

    edit some stuffs

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ var typed = new Typed('.text-2', {
 });
 
 // Function to the about section
-let tabLinks = document.getElementsByClassName('tab-links')
-let tabContents = document.getElementsByClassName('tab-contents')
+let tabLinks = document.getElementsByClassName('tab-links');
+let tabContents = document.getElementsByClassName('tab-contents');
 function opentab(tabName) {
     for (const tabLink of tabLinks) {
-        tabLink.classList.remove('active-link')
+        tabLink.classList.remove('active-link');
     }
     for (const tabContent of tabContents) {
-        tabContent.classList.remove('active-tab')
+        tabContent.classList.remove('active-tab');
     }
     event.currentTarget.classList.add('active-link')
     document.getElementById(tabName).classList.add('active-tab');
 }
 
 // Function for the menu icon
-let sidemenu = document.querySelector('#sidemenu')
+let sidemenu = document.querySelector('#sidemenu');
 function openMenu() {
-    sidemenu.style.right = "0"
+    sidemenu.style.right = "0";
 }
 function closeMenu() {
-    sidemenu.style.right = "-200px"
+    sidemenu.style.right = "-200px";
 }
 
 
@@ -38,14 +38,14 @@ const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById('msg');
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
+    e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
             msg.style.display = "block"
             setTimeout(() => {
                 msg.style.display = "none"
             }, 6000);
-            form.reset()
+            form.reset();
         })
-        .catch(error => console.error('Error!', error.message))
-})
+        .catch(error => console.error('Error!', error.message));
+});
